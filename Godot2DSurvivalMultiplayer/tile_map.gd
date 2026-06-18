ef756f2ec20d23bd42f757439e5c314afe19c178
SK4RK <sickdoggie776@gmail.com>
2026-06-18 11:25:13 +0100

    world gen works

--- Godot2DSurvivalMultiplayer/tile_map.gd
+++ Godot2DSurvivalMultiplayer/tile_map.gd
@@ -8,8 +8,8 @@ var tree_noise = FastNoiseLite.new()
 var tree_density_noise = FastNoiseLite.new()
 var spawn_noise = FastNoiseLite.new()
 
-@export var width =16
-@export var height = 16
+@export var width =128
+@export var height = 128
 var last_tile_pos = Vector2i(INF, INF)
 
 var tile_objects: Dictionary = {}
