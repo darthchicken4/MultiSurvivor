ddc70c2eca7392481825b8ee44465639a9dad503
SK4RK <sickdoggie776@gmail.com>
2026-06-18 11:36:46 +0100

    just clean up

--- Godot2DSurvivalMultiplayer/scenes/zombie/zombie.gd
+++ Godot2DSurvivalMultiplayer/scenes/zombie/zombie.gd
@@ -1,1 +1,33 @@
 extends RigidBody2D
+
+
+@export var player: CharacterBody2D
+
+
+#player priority is the there to avoid cheese when a single guy just runs  a
+#away wilst the other lob spears 
+var player_priority = 0
+
+
+func  _ready() -> void:
+	pass
+
+
+func _on_damage_area_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_damage_area_body_exited(body: Node2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_detect_area_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_detect_area_body_exited(body: Node2D) -> void:
+	pass # Replace with function body.
+
+
+func _process(delta: float) -> void:
+	pass
