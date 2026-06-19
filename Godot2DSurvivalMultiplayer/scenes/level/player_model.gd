067762d0175c053e781579098021fd280d809ccf
shcok-vlaue <sickdoggie776@gmail.com>
2026-06-19 18:10:32 +0200

    rot

--- Godot2DSurvivalMultiplayer/scenes/level/player_model.gd
+++ Godot2DSurvivalMultiplayer/scenes/level/player_model.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-
+var rotation_speed = 10
 @onready var head = $head
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,5 +8,10 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(delta):
+	var mouse_position = get_global_mouse_position()
+	var direction = mouse_position - global_position
+	var target_angle = direction.angle()
+	
+	# Interpolate current rotation towards target_angle smoothly
+	head.rotation = lerp_angle(rotation, target_angle, rotation_speed * delta)
