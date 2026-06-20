615373db4fedba608abe558e1e99eb69ea2b5096
shcok-vlaue <sickdoggie776@gmail.com>
2026-06-20 15:21:43 +0200

    sum ajusts

--- Godot2DSurvivalMultiplayer/scenes/caves/caves_prefab.gd
+++ Godot2DSurvivalMultiplayer/scenes/caves/caves_prefab.gd
@@ -8,7 +8,7 @@ var player: Node2D = null
 @export var seed = 1902837
 @export var dist = 7
 @export var degree = 0 
-var center  =Vector2(200,200) 
+
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	pass # Replace with function body.
@@ -26,13 +26,11 @@ func gen_cave():
 	var ray
 	#20000 area?
 	#ratio  8 to 272  1 34 
-	
 	for i in range(359):
 		degree = degree + 1
 		ray = Vector2(0,0)
-		ray = Vector2i(Vector2(dist * cos(degree) + cave_pos[0], dist * sin(degree) + cave_pos[1] ))
+		ray = Vector2i(Vector2(dist * cos(degree) + cave_pos[0] + randf_range(1.0,1.5) , dist * sin(degree) + cave_pos[1] + randf_range(1.0,1.5)  ))
 		exit_area.global_position = cave_pos * 34
-		 
 		tile.set_cell(ray,0,Vector2(1,1))
 	
 
