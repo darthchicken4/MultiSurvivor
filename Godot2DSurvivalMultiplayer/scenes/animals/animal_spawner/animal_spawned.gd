f37bc2e7d27dd9afb0883cf4cce5c542d3921990
shcok-vlaue <sickdoggie776@gmail.com>
2026-07-09 20:15:15 +0200

    bulk_spawner

--- Godot2DSurvivalMultiplayer/scenes/animals/animal_spawner/animal_spawned.gd
+++ Godot2DSurvivalMultiplayer/scenes/animals/animal_spawner/animal_spawned.gd
@@ -4,23 +4,29 @@ extends Node2D
 @export var chiken: PackedScene 
 @export var hog :PackedScene
 @export var tilemap_index = TileMapLayer
-
+@onready var multiplayer_spawner = $MultiplayerSpawner
 @export var animal_ammount  = 20
 
 @export var animal = 0
 func _ready() -> void:
 	spawn()
 	
+
 func spawn():
-	var spawn_pos = tilemap_index.grass_spawn
-	animal = randi_range(0, 1)
-	if animal == 0:
-		var hog_instance = hog.instantiate()
-		hog_instance.position = spawn_pos
-		add_child(hog_instance)
-		print(hog_instance.global_position)
-	elif animal == 1:
-		var chicken_instance = chiken.instantiate()
-		chicken_instance.position = spawn_pos
-		add_child(chicken_instance)
-		print(chicken_instance.global_position)
+	pass
+###
+	#if not multiplayer_spawner.is_server():
+	#	return
+
+	#var spawn_pos = tilemap_index.grass_spawn
+	#var animal = randi_range(0, 1)
+	#var instance: Node
+
+	#if animal == 0:
+	#	instance = hog.instantiate()
+	#else:
+	#	instance = chiken.instantiate()
+	#instance.name = "Animal_%d" % randi()
+	#instance.set_multiplayer_authority(1) # 1 = server authority
+	#instance.position = spawn_pos
+	#$AnimalContainer.add_child(instance, true) # true = force readable name, helps sync
