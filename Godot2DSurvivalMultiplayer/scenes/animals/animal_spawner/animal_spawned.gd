98917a762e1549c71f0b076a8d4e0995485d2ec8
shcok-vlaue <sickdoggie776@gmail.com>
2026-07-09 20:08:22 +0200

    animals_can spawm but not synced

--- Godot2DSurvivalMultiplayer/scenes/animals/animal_spawner/animal_spawned.gd
+++ Godot2DSurvivalMultiplayer/scenes/animals/animal_spawner/animal_spawned.gd
@@ -1,15 +1,26 @@
 extends Node2D
 
 
-@export var chiken = PackedScene 
-@export var hog  = PackedScene
+@export var chiken: PackedScene 
+@export var hog :PackedScene
 @export var tilemap_index = TileMapLayer
 
 @export var animal_ammount  = 20
 
-
+@export var animal = 0
 func _ready() -> void:
-	pass
+	spawn()
 	
 func spawn():
-	var spawn_pos = tilemap_index.grass_spawn 
+	var spawn_pos = tilemap_index.grass_spawn
+	animal = randi_range(0, 1)
+	if animal == 0:
+		var hog_instance = hog.instantiate()
+		hog_instance.position = spawn_pos
+		add_child(hog_instance)
+		print(hog_instance.global_position)
+	elif animal == 1:
+		var chicken_instance = chiken.instantiate()
+		chicken_instance.position = spawn_pos
+		add_child(chicken_instance)
+		print(chicken_instance.global_position)
