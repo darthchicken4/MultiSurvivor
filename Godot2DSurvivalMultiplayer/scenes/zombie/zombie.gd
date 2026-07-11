91b095c26b1e84fd27df086c7452b886177ee521
shcok-vlaue <sickdoggie776@gmail.com>
2026-07-11 18:19:27 +0200

    animation fix

--- Godot2DSurvivalMultiplayer/scenes/zombie/zombie.gd
+++ Godot2DSurvivalMultiplayer/scenes/zombie/zombie.gd
@@ -2,14 +2,11 @@ extends CharacterBody2D
 
 
 
-
-#player priority is the there to avoid cheese when a single guy just runs  a
-#away wilst the other lob spears 
-
 @export var speed: float = 50.0
 @export  var wander_dir = 3
 @export var wander_timer = 4
 
+@onready var anim = $AnimatedSprite2D
 
 
 
@@ -19,31 +16,45 @@ var player_pos = Vector2(0,0)
 var is_thinking = false
 var think_timer = 3.0
 var wander_direction = Vector2(0,0)
+
+
 func  _ready() -> void:
-	pass
+	set_multiplayer_authority(1)
 
 
 func _on_damage_area_body_entered(body: Node2D) -> void:
 	pass # Replace with function body.
 
 
-
 func _on_detect_area_body_entered(body: Node2D) -> void:
+	if not is_multiplayer_authority():
+		return
 	if body.is_in_group("Player"):
 		player = body
 
-
-
 func _on_detect_area_body_exited(body: Node2D) -> void:
+	if not is_multiplayer_authority():
+		return
 	if body == player:
 		player = null
 
 func _process(delta: float) -> void:
+	if not is_multiplayer_authority():
+		update_animation()
+		return
 	if player:
 		var direction = (player.global_position - global_position).normalized()
-		position += direction * speed * delta
-	if player == null:
+		velocity = direction * speed
+		global_position += velocity * delta
+	else:
+		velocity = Vector2.ZERO
 		wander(delta)
+
+	if velocity.length() > 0.1:
+		anim.play("run")
+	else:
+		anim.play("idle")
+
 
 
 func wander(delta: float) -> void:
@@ -60,13 +71,18 @@ func wander(delta: float) -> void:
 			is_thinking = false
 
 		return
-
 	# Moving
-	position += wander_direction * speed * delta
-
+	velocity = wander_direction * speed
+	global_position += velocity * delta
 	wander_timer -= delta
 
 	if wander_timer <= 0:
 		is_thinking = true
 		think_timer = randf_range(0.5, 2.0)
 		
+		
+func update_animation() -> void:
+	if velocity.length() > 0.1:
+		anim.play("run")
+	else:
+		anim.play("idle")
