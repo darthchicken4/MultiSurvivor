93ba1800b6348e1818dda331c73c1397bec2f00c
darthchicken4 <113773982+darthchicken4@users.noreply.github.com>
2026-07-26 19:54:40 +0100

    Broken Inspect

--- Godot2DSurvivalMultiplayer/scripts/propScripts/tree.gd
+++ Godot2DSurvivalMultiplayer/scripts/propScripts/tree.gd
@@ -4,14 +4,3 @@ var actions: Array[String] = ["Inspect"]
 var objectname : String = "Pine Tree"
 
 @export var inspect_text: String = "A sturdy pine tree, slightly twisted in some places."
-
-
-
-
-
-func _on_visible_on_screen_notifier_2d_screen_entered() -> void:
-	visible = true
-
-
-func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
-	visible = false
